d4b4d68bf0d7e60f0b457ea09647e0dbe5753b07
Heather Lemon <heather.lemon@canonical.com>
2020-07-09 15:30:50 -0600

    add encoding for sent messages

--- KafkaConsumer.py
+++ KafkaConsumer.py
@@ -3,17 +3,25 @@ from kafka import KafkaConsumer
 
 
 class KafkaTestConsumer:
-    bootstrap_servers = ['localhost:9092']
-    topic_name = 'bigdata_tweets'
+    bootstrap_servers = ['localhost:2888']
+    topic_name = 'kafka_tweets'
 
-    producer = KafkaConsumer(bootstrap_servers=bootstrap_servers)
-
-    ack = producer.send(topic_name)
-    metadata = ack.get()
-    print(metadata.topic)
-    print(metadata.partition)
-    print(metadata.offset)
+    def consume_messages(self):
+        try:
+            kafka_consumer = KafkaConsumer(self.topic_name, bootstrap_servers=self.bootstrap_servers)
+            print(kafka_consumer.bootstrap_connected())
+            if kafka_consumer is not None:
+                for message in kafka_consumer:
+                    # message value and key are raw bytes -- decode if necessary!
+                    # e.g., for unicode: `message.value.decode('utf-8')`
+                    print(message)
+                    # print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                    #                                      message.offset, message.key,
+                    #                                      message.value))
+        except Exception as ex:
+            print("Failure in consume messages", ex)
 
 
 if __name__ == "__main__":
-    KafkaTestConsumer()
+    kafka = KafkaTestConsumer()
+    kafka.consume_messages()
--- KafkaProducer.py
+++ KafkaProducer.py
@@ -1,31 +1,28 @@
 #!/usr.bin.python
 from kafka import KafkaProducer
 
-import json
-
 
 class KafkaTestProducer:
 
     def __init__(self):
-        bootstrap_servers = ['localhost:9092']
-        self.topic_name = 'kafka_tweets'
-        self.producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
-        print(self.producer.bootstrap_connected())
+        try:
+            bootstrap_servers = ['localhost:2888']
+            self.topic_name = 'kafka_tweets'
+            self.producer = KafkaProducer(bootstrap_servers=['localhost:2888'])
+            print(self.producer.bootstrap_connected())
+        except Exception as ex:
+            print("Error connecting to kafka.", str(ex))
+            self.producer.close()
 
     def get_twitter_stream(self, twitter_stream):
         for line in twitter_stream:
             try:
-                if line.get(id) is not None:
-                    key = line.get('id')
-                    value = line.get('text')
-                    key_bytes = bytes(str.encode(key))
-                    value_bytes = bytes(str.encode(value))
-                    record_metadata = self.producer.send(self.topic_name, key=key_bytes, value=value_bytes)
+                key = line.get('id')
+                value = line.get('text')
+                if key is not None and value is not None:
+                    self.producer.send(self.topic_name, str(key).encode("utf-8"),value.encode("utf-8"))
                     print("Message sent to kafka consumer")
-                    print(record_metadata.get('topic'))
-                    print(record_metadata.get('partition'))
-                    print(record_metadata.get('offset'))
-                    # self.producer.flush()
+                    self.producer.flush()
             except Exception as ex:
                 print("Error sending message to kafka.", str(ex))
         self.producer.close()
